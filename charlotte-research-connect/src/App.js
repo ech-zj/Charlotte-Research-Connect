b5267c693be6df7a5a121aef32d612cd44f1f986
Tcarr9442 <carrt313@gmail.com>
2021-10-28 14:37:24 -0400

    Website Update

--- charlotte-research-connect/src/App.js
+++ charlotte-research-connect/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './Pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" render={props => <HomePage {...props} />} />
+    </BrowserRouter>
   );
 }
 
---
+++ charlotte-research-connect/src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div className='App'>
+            <h1>Yes</h1>
+        </div>
+    )
+}
+
+export default HomePage
