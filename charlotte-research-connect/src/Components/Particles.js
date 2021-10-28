80bbf1ebd8074ac2458a7fe0af0b75a73aa04ab3
Tcarr9442 <carrt313@gmail.com>
2021-10-28 15:14:52 -0400

    Create Particles.js

---
+++ charlotte-research-connect/src/Components/Particles.js
@@ -0,0 +1,63 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import Particles from 'react-tsparticles'
+
+export default (props) => {
+    return (
+        <Particles
+            width='100vw'
+            height='100vh'
+            style={{ overflow: 'hidden' }}
+            options={{
+                background: {
+                    color: {
+                        value: "#2C2F33"
+                    },
+                },
+                fpsLimit: 60,
+                interactivity: {
+                    detectsOn: "window",
+                    events: {
+                        onHover: {
+                            enable: true,
+                            mode: "attract"
+                        },
+                        onClick: {
+                            enabled: true,
+                            mode: 'attract'
+                        }
+                    },
+                    modes: {
+                        attract: {
+                            quantity: .1
+                        },
+                        grab: {
+                            distance: 150
+                        }
+                    }
+                },
+                particles: {
+                    number: {
+                        value: 60,
+                    },
+                    color: {
+                        value: "#8730d9"
+                    },
+                    links: {
+                        color: "#99AAB5",
+                        distance: 50,
+                        enable: true,
+                        opacity: .5,
+                        width: 1
+                    },
+                    move: {
+                        enable: true,
+                        speed: .3,
+                        direction: 'random',
+                        random: true
+                    },
+                }
+            }}
+        />
+    )
+}
