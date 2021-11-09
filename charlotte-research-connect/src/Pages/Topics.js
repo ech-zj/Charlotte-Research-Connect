f905bc34cec05d9dc406ed880dd6a26d5b8b1bee
Tcarr9442 <carrt313@gmail.com>
2021-11-08 22:57:16 -0500

    Sub Topics Page

--- charlotte-research-connect/src/Pages/Topics.js
+++ charlotte-research-connect/src/Pages/Topics.js
@@ -7,6 +7,7 @@ const settings = require('../settings.json')
 function TopicPage(props) {
     const [topics, setTopics] = useState([])
     const [selectedTopic, setSelectedTopic] = useState(null)
+    const [subTopicList, setSubTopics] = useState([])
 
     useEffect(() => {
         const getTopics = async () => {
@@ -20,8 +21,27 @@ function TopicPage(props) {
 
     const renderTopic = (topic) => {
         return (
-            <h2 key={topic.id} className='Topic' onClick={() => { setSelectedTopic(topic.label) }}>{topic.label}</h2>
+            <h2 key={topic.id} className='Topic' onClick={() => selectTopic(topic)}>{topic.label}</h2>
         )
+    }
+
+    const renderSubTopic = (topic) => {
+        return (
+            <h2 key={topic.id} className='Topic' onClick={() => alert('yahoo')}>{topic.label}</h2>
+        )
+    }
+
+    const selectTopic = async topic => {
+        // Set selected topic state
+        setSelectedTopic(topic.label);
+
+        // Call API to get sub topics
+        const data = await axios.get(`${settings.DevEnv ? settings.NoSSL : settings.APIBase}/topics/sub/${topic.id}`)
+            .catch(er => { return { isErrored: true, er } })
+        if (data.isErrored || !data.data) setTopics({ errored: true })
+
+        // assume that data.data is an array
+        setSubTopics(data.data)
     }
 
     return (<>
@@ -33,18 +53,16 @@ function TopicPage(props) {
                     <h1>{selectedTopic ? 'Back To Topics' : 'Topics'}</h1>
                 </div>
                 <div className='break' />
+                <hr style={{ margin: '1rem', width: '60vw' }} />
+                <div className='break' />
                 {topics.errored ?
                     <h2>Error Getting Topics</h2> :
                     selectedTopic ? <>
-                        <hr style={{ margin: '1rem', width: '60vw' }} />
-                        <div className='break' />
                         <h1>You Selected {selectedTopic}</h1>
-                        <div className='break' style={{paddingTop: '3   rem'}} />
-                        <h1>This implementation will be coming soon</h1>
+                        <div className='break' style={{ paddingTop: '3rem' }} />
+                        {subTopicList.map(t => { return renderSubTopic(t) })}
                     </> :
-                        topics.map(t => {
-                            return renderTopic(t)
-                        })
+                        topics.map(t => { return renderTopic(t) })
                 }
             </div>
         </div>
