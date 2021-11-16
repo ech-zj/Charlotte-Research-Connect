11066c9da70c0266569df1bdd141dc0c972a2f70
Tcarr9442 <carrt313@gmail.com>
2021-11-15 19:33:45 -0500

    Main Visualization Created, unfinished

--- charlotte-research-connect/src/Pages/Home.js
+++ charlotte-research-connect/src/Pages/Home.js
@@ -1,18 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../css/Home.css'
 import PageTemplate from '../Components/PageTemplate'
+import Graph from "react-graph-vis";
+import axios from 'axios';
+const settings = require('../settings.json')
 
 function HomePage(props) {
+    const [data, setData] = useState(null)
+    const [viewFaculty, setViewFaculty] = useState(false)
+
+    useEffect(() => {
+        async function getData() {
+            const data = await axios.get(`${settings.DevEnv ? settings.NoSSL : settings.APIBase}${viewFaculty ? '/visuals/faculty' : '/visuals/main'}`)
+                .catch(er => { return { isErrored: true, er } })
+            console.log(data)
+            if (data.isErrored || !data.data || !data.data.data) { console.log(data.er.response); setData({ errored: true }) }
+            else setData(data.data.data)
+        }
+        getData()
+    }, [])
+
     return (<>
         <PageTemplate highLight='0' {...props} />
         <div className='HomePage'>
             <div className='Visualization'>
-                <h1 style={{ display: 'flex', alignItems: 'center', height: '80vh' }}>Main Visualization Will Be Here</h1>
-                <div className='break' />
-                <h2>Instructions on navigating viusalization</h2>
+                {data ?
+                    <Graph style={{ height: '95vh', width: '100vw' }} graph={data} options={options} />
+                    :
+                    <h1 style={{ display: 'flex', alignItems: 'center', height: '80vh' }}>Loading...</h1>
+                }
             </div>
         </div>
     </>)
 }
 
 export default HomePage
+
+const options = {
+    autoResize: true,
+    clickToUse: true,
+    edges: {
+        color: "#000000"
+    },
+    groups: {
+        'college-of-business': {},
+        'college-of-arts-and-architecture': {},
+        'cci': {},
+        'coed': {},
+        'chhs': {},
+        'clas': {},
+        'college-of-engineering': {}
+    },
+    interaction: {
+        hideEdgesOnDrag: true,
+        hideEdgesOnZoom: true,
+        selectable: false,
+        navigationButtons: true,
+    },
+    physics: {
+        barnesHut: {
+            centralGravity: 0.0001,
+            avoidOverlap: .65
+        },
+        repulsion: {
+            centralGravity: 0.0001
+        }
+    }
+}
+
