727169fc3fe75fa23d1e829e7be4b3aa3f66282f
Thomas Carr <35206339+Tcarr9442@users.noreply.github.com>
2021-12-10 23:44:34 -0500

    Login update

--- charlotte-research-connect/src/Pages/Login.js
+++ charlotte-research-connect/src/Pages/Login.js
@@ -3,14 +3,18 @@ import GoogleLogin from 'react-google-login'
 import { useHistory } from 'react-router-dom';
 import PageTemplate from '../Components/PageTemplate'
 import { ReactComponent as GoogleLogo } from '../Google.svg';
-import UserService from '../Services/User'
+// import UserService from '../Services/User'
 import '../css/Login.css'
 
 function LoginPage(props) {
     const history = useHistory()
     async function handleLogin(jw) {
-        UserService.verify(jw.tokenId)
-        history.push('/a/home')
+        // UserService.verify(jw.tokenId)
+        // history.push('/a/home')
+        if (jw.vu ) {
+            if(jw.vu.jf)localStorage.setItem('name', jw.vu.jf)
+            history.push('/a/home')
+        }
     }
 
 
@@ -18,15 +22,15 @@ function LoginPage(props) {
         <div>
             <PageTemplate />
             <div className='LoginPanel'>
-                <div style={{width:'250px', height: '250px', marginBottom:'4rem'}}><GoogleLogo/></div>
+                <div style={{ width: '250px', height: '250px', marginBottom: '4rem' }}><GoogleLogo /></div>
                 <GoogleLogin
                     render={renderProps => (
                         <button onClick={renderProps.onClick}>Login with Google</button>
                     )}
                     clientId='128477008433-0qrl14k83ljq8ih49lbmdn6hibnh1bd1.apps.googleusercontent.com'
                     buttonText="Login with Google"
-                    onSuccess={handleLogin}
-                    onFailure={handleLogin}
+                    onSuccess={(props) => handleLogin(props)}
+                    onFailure={(props) => handleLogin(props)}
                     cookiePolicy={'single_host_origin'}
                     redirectUri='https://charlotte-research-connect.web.app/a/home'
                 />
